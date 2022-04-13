b5b52e26965d03082e0cda4e547f9cea0f611eed
E <e@pastel.codes>
2022-04-13 21:16:26 +0100

    Add offset to mvc URI

--- application/mvc.php
+++ application/mvc.php
@@ -5,7 +5,8 @@ require 'controller/controller.php';
 $pageURI = $_SERVER['REQUEST_URI'];
 
 // Get URI end of the requested URI
-$pageURI = substr($pageURI, strrpos($pageURI, 'index.php') + 1);
+// $pageURI = substr($pageURI, strrpos($pageURI, 'index.php') + 1); // Localhost only
+$pageURI = substr($pageURI, strrpos($pageURI, 'index.php') + 24); // ITS server
 
     // Load controller response depending if index or other
     if (!$pageURI) {
