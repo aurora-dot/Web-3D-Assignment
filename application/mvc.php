95c8be7ddaeddef97e5cf2539aa3926c25fffd77
E <e@pastel.codes>
2022-04-13 21:17:49 +0100

    Add to offset

--- application/mvc.php
+++ application/mvc.php
@@ -6,7 +6,7 @@ $pageURI = $_SERVER['REQUEST_URI'];
 
 // Get URI end of the requested URI
 // $pageURI = substr($pageURI, strrpos($pageURI, 'index.php') + 1); // Localhost only
-$pageURI = substr($pageURI, strrpos($pageURI, 'index.php') + 24); // ITS server
+$pageURI = substr($pageURI, strrpos($pageURI, 'index.php') + 27); // ITS server
 
     // Load controller response depending if index or other
     if (!$pageURI) {
